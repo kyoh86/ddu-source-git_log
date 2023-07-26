20496250fa06301ffd3905e29c4f9c33bc10a2c6
kyoh86 <me@kyoh86.dev>
2023-07-27 02:19:40 +0900

    add "yank" action

--- denops/@ddu-kinds/git_commit.ts
+++ denops/@ddu-kinds/git_commit.ts
@@ -9,6 +9,10 @@ import type { DduItem } from "https://deno.land/x/ddu_vim@v3.4.2/types.ts";
 import { passthrough } from "../ddu-source-git_log/message.ts";
 import { GetPreviewerArguments } from "https://deno.land/x/ddu_vim@v3.4.3/base/kind.ts";
 import { ensure, is } from "https://deno.land/x/unknownutil@v3.2.0/mod.ts";
+import {
+  setreg,
+} from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
+import { v } from "https://deno.land/x/denops_std@v5.0.1/variable/mod.ts";
 
 export type ActionData = {
   cwd: string;
@@ -22,20 +26,24 @@ export type ActionData = {
 
 type Params = Record<never, never>;
 
-async function callProcessByCommit(
-  denops: Denops,
-  items: DduItem[],
-  subargs: string[],
-) {
+async function ensureOnlyOneItem(denops: Denops, items: DduItem[]) {
   if (items.length != 1) {
     await denops.call(
       "ddu#util#print_error",
       "invalid action calling: it can accept only one item",
       "ddu-kind-git_commit",
     );
-    return ActionFlags.None;
+    return;
   }
-  const action = items[0].action as ActionData;
+  return items[0];
+}
+
+function callProcessByCommit(
+  denops: Denops,
+  item: DduItem,
+  subargs: string[],
+) {
+  const action = item.action as ActionData;
   const args = [
     ...subargs,
     action.hash,
@@ -50,7 +58,15 @@ async function callProcessByCommit(
       stdout: "piped",
     }).spawn(),
   );
-  return ActionFlags.None;
+}
+
+function getHash(actionParams: unknown, item?: DduItem) {
+  const params = ensure(actionParams, is.Record);
+  const length = ("length" in params) ? ensure(params.hard, is.Number) : 0;
+
+  const action = item?.action as ActionData;
+  return length > 0 ? action.hash.substring(0, length) : action.hash;
+}
 }
 
 export class Kind extends BaseKind<Params> {
@@ -58,14 +74,31 @@ export class Kind extends BaseKind<Params> {
     string,
     (args: ActionArguments<Params>) => Promise<ActionFlags>
   > = {
-    reset: async (args) => {
-      const params = ensure(args.actionParams, is.Record);
+    reset: async ({ actionParams, items, denops }) => {
+      const item = await ensureOnlyOneItem(denops, items);
+      if (!item) {
+        return ActionFlags.None;
+      }
+      const params = ensure(actionParams, is.Record);
       const hard = ("hard" in params) && ensure(params.hard, is.Boolean);
       const gitArgs = ["reset"];
       if (hard) {
         gitArgs.push("--hard");
       }
-      await callProcessByCommit(args.denops, args.items, gitArgs);
+      callProcessByCommit(denops, item, gitArgs);
+      return ActionFlags.None;
+    },
+
+    yank: async ({ actionParams, items, denops }) => {
+      const item = await ensureOnlyOneItem(denops, items);
+      if (!item) {
+        return ActionFlags.None;
+      }
+      const hash = getHash(actionParams, item);
+
+      await setreg(denops, '"', hash, "v");
+      await setreg(denops, await v.get(denops, "register"), hash, "v");
+
       return ActionFlags.None;
     },
   };

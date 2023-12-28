2b30e97fbabb3a51dc2be475e8915be9130b42ee
kyoh86 <me@kyoh86.dev>
2023-12-29 07:14:34 +0900

    Update dependencies

--- denops/@ddu-kinds/git_commit.ts
+++ denops/@ddu-kinds/git_commit.ts
@@ -1,19 +1,19 @@
-import type { Denops } from "https://deno.land/x/ddu_vim@v3.8.1/deps.ts";
+import type { Denops } from "https://deno.land/x/ddu_vim@v3.9.0/deps.ts";
 import {
   ActionFlags,
   BaseKind,
-} from "https://deno.land/x/ddu_vim@v3.8.1/types.ts";
+} from "https://deno.land/x/ddu_vim@v3.9.0/types.ts";
 import type {
   Actions,
   Previewer,
-} from "https://deno.land/x/ddu_vim@v3.8.1/types.ts";
-import type { DduItem } from "https://deno.land/x/ddu_vim@v3.8.1/types.ts";
+} from "https://deno.land/x/ddu_vim@v3.9.0/types.ts";
+import type { DduItem } from "https://deno.land/x/ddu_vim@v3.9.0/types.ts";
 import { pipe } from "https://denopkg.com/kyoh86/denops_util@v0.0.1/pipe.ts";
 import { yank } from "https://denopkg.com/kyoh86/denops_util@v0.0.1/yank.ts";
 import { put } from "https://denopkg.com/kyoh86/denops_util@v0.0.1/put.ts";
-import type { GetPreviewerArguments } from "https://deno.land/x/ddu_vim@v3.8.1/base/kind.ts";
+import type { GetPreviewerArguments } from "https://deno.land/x/ddu_vim@v3.9.0/base/kind.ts";
 import { ensure, is } from "https://deno.land/x/unknownutil@v3.11.0/mod.ts";
-import { fn } from "https://deno.land/x/ddu_vim@v3.8.1/deps.ts";
+import { fn } from "https://deno.land/x/ddu_vim@v3.9.0/deps.ts";
 
 export type ActionData = {
   kind: "commit";

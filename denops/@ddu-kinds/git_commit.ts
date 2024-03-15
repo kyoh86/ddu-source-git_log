53d600302958c1932906d7bc1b00213d897329ac
GitHub <noreply@github.com>
2024-03-15 09:03:08 +0000

    chore(deps): Update deno dependencies

--- denops/@ddu-kinds/git_commit.ts
+++ denops/@ddu-kinds/git_commit.ts
@@ -8,9 +8,9 @@ import type {
   Previewer,
 } from "https://deno.land/x/ddu_vim@v3.10.3/types.ts";
 import type { DduItem } from "https://deno.land/x/ddu_vim@v3.10.3/types.ts";
-import { echoallCommand } from "https://denopkg.com/kyoh86/denops-util@v0.0.6/command.ts";
-import { yank } from "https://denopkg.com/kyoh86/denops-util@v0.0.6/yank.ts";
-import { put } from "https://denopkg.com/kyoh86/denops-util@v0.0.6/put.ts";
+import { echoallCommand } from "https://denopkg.com/kyoh86/denops-util@v0.0.7/command.ts";
+import { yank } from "https://denopkg.com/kyoh86/denops-util@v0.0.7/yank.ts";
+import { put } from "https://denopkg.com/kyoh86/denops-util@v0.0.7/put.ts";
 import type { GetPreviewerArguments } from "https://deno.land/x/ddu_vim@v3.10.3/base/kind.ts";
 import { ensure, is } from "https://deno.land/x/unknownutil@v3.17.0/mod.ts";
 import { fn } from "https://deno.land/x/ddu_vim@v3.10.3/deps.ts";

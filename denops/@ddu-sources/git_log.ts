ee1383cd853b94f686723f8468e6b3d5ebe3d094
kyoh86 <me@kyoh86.dev>
2024-04-11 21:38:53 +0900

    Update dependencies

--- denops/@ddu-sources/git_log.ts
+++ denops/@ddu-sources/git_log.ts
@@ -2,7 +2,7 @@ import type { GatherArguments } from "https://deno.land/x/ddu_vim@v3.10.3/base/s
 import { fn } from "https://deno.land/x/ddu_vim@v3.10.3/deps.ts";
 import { treePath2Filename } from "https://deno.land/x/ddu_vim@v3.10.3/utils.ts";
 import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.10.3/types.ts";
-import { TextLineStream } from "https://deno.land/std@0.221.0/streams/text_line_stream.ts";
+import { TextLineStream } from "https://deno.land/std@0.222.1/streams/text_line_stream.ts";
 import { ChunkedStream } from "https://deno.land/x/chunked_stream@0.1.4/mod.ts";
 
 import { ActionData } from "../@ddu-kinds/git_commit.ts";

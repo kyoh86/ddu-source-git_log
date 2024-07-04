a0ed5b2bbad6ce7cdd48d2ba6bee589d12fd1c38
kyoh86 <me@kyoh86.dev>
2024-07-05 02:16:20 +0900

    Update dependencies

--- denops/@ddu-kinds/git_commit.ts
+++ denops/@ddu-kinds/git_commit.ts
@@ -1,11 +1,11 @@
-import type { Denops } from "https://deno.land/x/ddu_vim@v4.1.0/deps.ts";
+import type { Denops } from "https://deno.land/x/ddu_vim@v4.1.1/deps.ts";
 import {
   ActionFlags,
   type Actions,
   BaseKind,
   type Previewer,
 } from "https://deno.land/x/ddu_vim@v4.1.1/types.ts";
-import type { DduItem } from "https://deno.land/x/ddu_vim@v4.1.0/types.ts";
+import type { DduItem } from "https://deno.land/x/ddu_vim@v4.1.1/types.ts";
 import { echoallCommand } from "https://denopkg.com/kyoh86/denops-util@master/command.ts";
 import { yank } from "https://denopkg.com/kyoh86/denops-util@master/yank.ts";
 import { put } from "https://denopkg.com/kyoh86/denops-util@master/put.ts";
